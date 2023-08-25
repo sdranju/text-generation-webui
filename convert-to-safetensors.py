2aff9cffd8fdfb3d138f6b1dee3a74e2b2ea1105
Shamsuddoha Ranju <sdranju@gmail.com>
2023-08-25 22:15:03 +0600

    Update convert-to-safetensors.py

--- convert-to-safetensors.py
+++ convert-to-safetensors.py
@@ -11,28 +11,44 @@ https://gist.github.com/81300/fe5b08bff1cba45296a829b9d6b0f303
 
 '''
 
+import sys
 import argparse
 from pathlib import Path
 
 import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
-parser = argparse.ArgumentParser(formatter_class=lambda prog: argparse.HelpFormatter(prog, max_help_position=54))
-parser.add_argument('MODEL', type=str, default=None, nargs='?', help="Path to the input model.")
-parser.add_argument('--output', type=str, default=None, help='Path to the output folder (default: models/{model_name}_safetensors).')
-parser.add_argument("--max-shard-size", type=str, default="2GB", help="Maximum size of a shard in GB or MB (default: %(default)s).")
-parser.add_argument('--bf16', action='store_true', help='Load the model with bfloat16 precision. Requires NVIDIA Ampere GPU.')
-args = parser.parse_args()
 
 if __name__ == '__main__':
-    path = Path(args.MODEL)
+
+    parser = argparse.ArgumentParser(formatter_class=lambda prog: argparse.HelpFormatter(prog, max_help_position=54))
+    parser.add_argument('MODEL', type=str, default=None, nargs='?', help="Path to the input model.")
+    parser.add_argument('--output', type=str, default=None, help='Path to the output folder (default: models/{model_name}_safetensors).')
+    parser.add_argument("--max-shard-size", type=str, default=None, help="Maximum size of a shard in GB or MB, i.e. '2GB' (default: %(default)s).")
+    parser.add_argument('--bf16', action='store_true', help='Load the model with bfloat16 precision. Requires NVIDIA Ampere GPU.')
+    args = parser.parse_args()
+
+    model_path = args.MODEL
+
+    if model_path is None:
+        print("Error: Please specify the model you'd like to convert (e.g. 'python convert-to-safetensors.py "
+              "models/Deci_DeciCoder-1b').")
+        sys.exit()
+
+    path = Path(model_path)
     model_name = path.name
+    max_shard_size = args.max_shard_size
 
     print(f"Loading {model_name}...")
     model = AutoModelForCausalLM.from_pretrained(path, low_cpu_mem_usage=True, torch_dtype=torch.bfloat16 if args.bf16 else torch.float16)
     tokenizer = AutoTokenizer.from_pretrained(path)
 
     out_folder = args.output or Path(f"models/{model_name}_safetensors")
-    print(f"Saving the converted model to {out_folder} with a maximum shard size of {args.max_shard_size}...")
-    model.save_pretrained(out_folder, max_shard_size=args.max_shard_size, safe_serialization=True)
+    if max_shard_size is not None:
+        print(f"Saving the converted model to {out_folder} with a maximum shard size of {args.max_shard_size}...")
+        model.save_pretrained(out_folder, max_shard_size=args.max_shard_size, safe_serialization=True)
+    else:
+        print(f"Saving the converted model to {out_folder}...")
+        model.save_pretrained(out_folder, safe_serialization=True)
+
     tokenizer.save_pretrained(out_folder)
